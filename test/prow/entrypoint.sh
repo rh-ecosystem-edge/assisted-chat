af15c0f6b95264baf3efdaedd90492c2bb4f4aac
aantal <aantal@redhat.com>
2025-09-08 21:33:41 +0200

    adding pull secret so the evaluation tests would succeed

--- test/prow/entrypoint.sh
+++ test/prow/entrypoint.sh
@@ -14,6 +14,10 @@ OCM_TOKEN=$(curl -X POST https://sso.redhat.com/auth/realms/redhat-external/prot
 echo "$OCM_TOKEN" > test/evals/ocm_token.txt
 echo "GEMINI_API_KEY=${GEMINI_API_KEY}" > .env
 
+mkdir -p ~/.config/containers
+curl -H "Authorization: Bearer ${OCM_TOKEN}" -X POST https://api.stage.openshift.com/api/accounts_mgmt/v1/access_token > ~/.config/containers/auth.json
+sleep 3600
+
 cd test/evals
 
 python eval.py --agent_endpoint "${AGENT_URL}:${AGENT_PORT}"
