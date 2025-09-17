88b3e00befc289531eb9adbcf6392b995713bde0
aantal <aantal@redhat.com>
2025-09-17 15:34:35 +0200

    an experiment for more stable eval tests

--- scripts/eval_k8s.sh
+++ scripts/eval_k8s.sh
@@ -17,5 +17,11 @@ fi
 bash "$PROJECT_ROOT/utils/port_forward.sh"
 
 # Run evaluation
+UNIQUE_ID=$(head /dev/urandom | tr -dc 0-9a-z | head -c 8)
+export UNIQUE_ID
+export TEMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TEMP_DIR"' EXIT
+cp test/evals/eval_data.yaml "${TEMP_DIR}/eval_data.yaml"
+sed -i "s/uniq-cluster-name/${UNIQUE_ID}/g" "${TEMP_DIR}/eval_data.yaml"
 cd "${PROJECT_ROOT}/test/evals"
-python eval.py --agent_endpoint "http://localhost:${ASSISTED_CHAT_PORT}" 
+python eval.py --agent_endpoint "http://localhost:${ASSISTED_CHAT_PORT}" --eval_data_yaml "${TEMP_DIR}/eval_data.yaml"
