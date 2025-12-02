05d2ca5e0eb0cab502d499b0acde0163a9cc3717
Eran Cohen <eranco@redhat.com>
2025-12-02 17:13:50 +0200

    feat(eval): Add conditional tag support for CI executions
    The evaluation entrypoint now defaults to `--tag smoke` unless the $TAG environment variable is explicitly set. If $TAG is set to 'all', no tag is passed.
    
    Signed-off-by: Eran Cohen <eranco@redhat.com>

--- test/prow/entrypoint.sh
+++ test/prow/entrypoint.sh
@@ -27,4 +27,17 @@ cp $TEST_DIR/eval_data.yaml $TEMP_DIR/eval_data.yaml
 sed -i "s/uniq-cluster-name/${UNIQUE_ID}/g" $TEMP_DIR/eval_data.yaml
 sed -i "s|: ../scripts|: ${WORK_DIR}/test/scripts|g" $TEMP_DIR/eval_data.yaml
 
-python $TEST_DIR/eval.py --agent_endpoint "${AGENT_URL}:${AGENT_PORT}" --agent_auth_token_file $TEMP_DIR/ocm_token.txt --eval_data_yaml $TEMP_DIR/eval_data.yaml --judge_provider="vertex"
+TAG_ARG=""
+
+if [[ -n "${TAG:-}" ]]; then
+  # $TAG is defined and not empty
+  if [[ "$TAG" != "all" ]]; then
+    # Use the specified $TAG, unless it is "all"
+    TAG_ARG="--tags "$TAG""
+  fi
+else
+  # $TAG is NOT defined, use the default "--tag smoke"
+  TAG_ARG="--tags smoke"
+fi
+
+python $TEST_DIR/eval.py $TAG_ARG --agent_endpoint "${AGENT_URL}:${AGENT_PORT}" --agent_auth_token_file $TEMP_DIR/ocm_token.txt --eval_data_yaml $TEMP_DIR/eval_data.yaml --judge_provider="vertex"
